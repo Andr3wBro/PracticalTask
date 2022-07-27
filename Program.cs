f9de0ad25ae31010b04bbc212abd69e9c0c2f55d
Andr3wBro <andr3wbro@gmail.com>
2022-07-27 13:22:09 +0300

    addPrintArray

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string[] array = { "Moscow", "NewYork", "<3", "FBI", "UNO" };
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+"  ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array);

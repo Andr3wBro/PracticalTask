faf146f641765cc570701b6fb3541202ee51c605
Andr3wBro <andr3wbro@gmail.com>
2022-07-27 13:27:01 +0300

    MainTaskDone

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
-﻿string[] array = { "Moscow", "NewYork", "<3", "FBI", "UNO" };
-
-void PrintArray(string[] array)
+﻿void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(array[i]+"  ");
+        Console.Write(array[i] + "  ");
     }
     Console.WriteLine();
 }
 
+string[] NewArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] newArray = new string[count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+
+string[] array = { "Moscow", "NewYork", "<3", "FBI", "UNO" };
 PrintArray(array);
+string[] newArray = NewArray(array);
+PrintArray(newArray);
